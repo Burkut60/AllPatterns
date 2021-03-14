427af5f9a1a28eda57e49417e064c799950aa3da
= <=>
2021-03-14 15:10:44 +0300

    test

---
+++ src/abstractFactory/main.java
@@ -0,0 +1,7 @@
+package abstractFactory;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("A" );
+    }
+}
---
+++ src/fgsf/main.java
@@ -0,0 +1,7 @@
+package fgsf;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(" dd");
+    }
+}
